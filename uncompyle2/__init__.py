bb418b51bde4c4e3b14fd3f3c61ee4016a28ae7e
wibiti <wibiti@live.com>
2011-07-23 18:50:05 -0500

    tweak

--- uncompyle2/__init__.py
+++ uncompyle2/__init__.py
@@ -138,7 +138,7 @@ else:
         return ''
 
 def main(in_base, out_base, files, codes, outfile=None,
-         showasm=0, showast=0, do_verify=0, showtotal=True):
+         showasm=0, showast=0, do_verify=0):
     """
     in_base	base directory for input files
     out_base	base directory for output files (ignored when
@@ -218,7 +218,4 @@ def main(in_base, out_base, files, codes, outfile=None,
             else:
                 okay_files += 1
                 print "+++ okay decompyling", infile, __memUsage()
-    if showtotal:
-        print '# decompiled %i files: %i okay, %i failed, %i verify failed' % \
-              (tot_files, okay_files, failed_files, verify_failed_files)
-    return (tot_files, okay_files, failed_files, verify_failed_files) 
+    return (tot_files, okay_files, failed_files, verify_failed_files)
