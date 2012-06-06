67c6d51849cc496cb57e726836204b921385e13a
wibiti <wibiti@live.com>
2012-06-06 10:23:32 -0500

    bug fix

--- uncompyle2/Walker.py
+++ uncompyle2/Walker.py
@@ -258,6 +258,7 @@ TABLE_DIRECT = {
 
     'whilestmt':	( '%|while %c:\n%+%c%-\n\n', 1, 2 ),
     'while1stmt':	( '%|while 1:\n%+%c%-\n\n', 1 ),
+    'while1elsestmt':	( '%|while 1:\n%+%c%-%|else:\n%+%c%-\n\n', 1, 3 ),
     'whileelsestmt':	( '%|while %c:\n%+%c%-%|else:\n%+%c%-\n\n', 1, 2, -2 ),
     'whileelselaststmt':	( '%|while %c:\n%+%c%-%|else:\n%+%c%-', 1, 2, -2 ),
     'forstmt':		( '%|for %c in %c:\n%+%c%-\n\n', 3, 1, 4 ),
